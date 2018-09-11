bcd373819af79e51e1933edb4ffe3a05ac2de3ec
eduardo lopes <glaysoneduardo@gmail.com>
2018-09-11 20:19:18 -0300

    tde

---
+++ calc.py
@@ -0,0 +1,16 @@
+def soma(a, b):
+        result = ( a+b )
+        return result
+
+def sub(c, d):
+	result = ( c-d ) 
+	return result
+
+def mult(e, f):
+	result = ( e*f )
+	return result
+
+def div(g, h):
+	result = ( g/h )
+	return result
+
---
+++ test_calc.py
@@ -0,0 +1,26 @@
+import pytest
+
+from calc import soma, sub, mult, div
+
+def test_soma():
+	
+	assert soma(2, 2) == 4
+	assert soma(-4, 8) == 4
+
+
+def test_sub():
+
+	assert sub(5, 2) == 3
+	assert sub(2, 2) == 0
+
+
+def test_mult():
+	
+	assert mult(5, 5) == 25
+	assert mult(3, 3) == 9
+
+
+def test_div():
+	assert div(6, 2) == 3
+	assert div(4, 1) == 4
+
